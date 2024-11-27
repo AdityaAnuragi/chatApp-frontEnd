cf91e045f9bbbfb1be22e15489ce523cb4fcdd30
AdityaAnuragi <110617126+AdityaAnuragi@users.noreply.github.com>
2024-11-27 20:45:52 +0530

    made request

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { useEffect, useState } from "react"
+
 function App() {
-  return <h1>hello world</h1>
+
+  const [msg, setMsg] = useState("hello world")
+
+  useEffect(() => {
+    fetch('http://localhost:3000')
+      // .then(res => res.json())
+      .then(value => value.text())
+      .then(value => setMsg(value))
+  },[])
+
+  return <h1>{msg}</h1>
 }
 
 export default App
