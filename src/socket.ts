e39a4a7e5e15513a32d3ae3445f49d9e3af52ec8
AdityaAnuragi <110617126+AdityaAnuragi@users.noreply.github.com>
2024-12-03 20:40:29 +0530

    added types to Socket object

--- src/socket.ts
+++ src/socket.ts
@@ -1,8 +1,16 @@
-import { io } from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = 'http://localhost:3000';
 
-export const socket = io(URL, {
+type ServerToClientEvents = {
+  message: (sender: string, id: number, message : string) => void;
+}
+
+type ClientToServerEvents = {
+  message: (sender:string, id:number, msg:string) => void
+}
+
+export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(URL, {
   autoConnect: false
 });
