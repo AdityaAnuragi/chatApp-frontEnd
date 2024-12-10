0994f2aa145c4e3570fb8de111ff64c4095f7cb3
AdityaAnuragi <110617126+AdityaAnuragi@users.noreply.github.com>
2024-12-10 20:56:09 +0530

    need to make it work with strict mode, currently it works only WITHOUT strict mode, most likely need to change some useEffect dependecy

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
-    <App />,
+  // TODO: make it work with the damm strict mode!!!!!!!!!!!!!!!!
+  <StrictMode>
+    <App />
   {/* </StrictMode>, */}
 )
