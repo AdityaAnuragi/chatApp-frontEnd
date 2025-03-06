99146c2c6ee7c8f615d202c983242ab6bb8451fb
AdityaAnuragi <110617126+AdityaAnuragi@users.noreply.github.com>
2025-03-06 19:32:06 +0530

    it will ask to remember password, kinda

--- src/loginAndSignUp/LoginAndSignUp.tsx
+++ src/loginAndSignUp/LoginAndSignUp.tsx
@@ -10,10 +10,7 @@ export function LoginAndSignUp({ logInUser, name, setName }: { logInUser: (userI
   const [isLoading, setIsLoading] = useState(false)
   // sign in functionality
 
-  function handleClick() {
-
-    setError("");
-    setIsLoading(true);
+  function signUpOrLogin() {
     (async () => {
       try {
         // make the URL as a templete literal string, /signup or /login
@@ -59,9 +56,59 @@ export function LoginAndSignUp({ logInUser, name, setName }: { logInUser: (userI
     })()
   }
 
+  function handleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.preventDefault()
+    setError("");
+    setIsLoading(true);
+    signUpOrLogin()
+    // (async () => {
+    //   try {
+    //     // make the URL as a templete literal string, /signup or /login
+    //     const response = await fetch(`https://chatapp-server-3x2r.onrender.com/${isSignUp ? "signup" : "signin"}`, {
+    //       method: "POST",
+    //       body: JSON.stringify({
+    //         name,
+    //         password
+    //       }),
+    //       headers: {
+    //         "Content-type": "application/json; charset=UTF-8"
+    //       }
+    //     })
+
+    //     if (response.status === 406) {
+    //       // incase username is already taken
+    //       throw new Error("406");
+    //     }
+    //     else if (!(response.ok)) {
+    //       throw new Error("An unexpected error occured")
+    //     }
+
+    //     const userId = await response.json()
+    //     // console.log(`user id is ${userId}`)
+    //     // console.log(response.ok)
+    //     logInUser(Number(userId))
+    //     setIsLoading(false)
+    //     setError("")
+    //   }
+    //   catch (e) {
+    //     if (e instanceof Error && e.message === "406") {
+    //       // console.log(e.name)
+    //       // console.log(e.stack)
+    //       // console.log(e.message)
+    //       setError("username taken")
+    //     }
+    //     else {
+    //       setError("unexpected error")
+    //     }
+    //     setIsLoading(false)
+    //   }
+
+    // })()
+  }
+
   function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
     if (e.key === "Enter") {
-      handleClick()
+      signUpOrLogin()
     }
   }
 
@@ -71,12 +118,12 @@ export function LoginAndSignUp({ logInUser, name, setName }: { logInUser: (userI
         <h2 className={styles.loginOrSignUp} >{isSignUp ? "Sign up" : "Login"}</h2>
         <label className={styles.label} >
           Name
-          <input className={styles.inputFields} value={name} onChange={e => setName(e.target.value)} type="text" placeholder="eg: Aditya" maxLength={100} />
+          <input name="username" type="text" className={styles.inputFields} value={name} onChange={e => setName(e.target.value)}  placeholder="eg: Aditya" maxLength={100} />
         </label>
 
         <label className={styles.label} >
           Password
-          <input className={styles.inputFields} value={password} onChange={e => setPassword(e.target.value)} onKeyDown={handleKeyDown} type="password" />
+          <input name="password" type="password" autoComplete="new-password" className={styles.inputFields} value={password} onChange={e => setPassword(e.target.value)} onKeyDown={handleKeyDown} />
         </label>
 
         <p className={`${error ? "" : styles.invisible} ${styles.ifUserNameTaken}`} >
@@ -87,10 +134,14 @@ export function LoginAndSignUp({ logInUser, name, setName }: { logInUser: (userI
               : "An unexpected error occured"
           }
         </p>
-        
-        <button className={`${styles.toggleBetweenLoginAndSignUp}`} onClick={() => {setIsSignUp(curr => !curr);setError("")}} >{isSignUp ? "Existing user? Sign in" : "Not registered? Sign up"}</button>
 
-        <button className={`${styles.loginOrSignUpButton} ${styles.right}`} onClick={handleClick} >
+        <button className={`${styles.toggleBetweenLoginAndSignUp}`} onClick={() => { setIsSignUp(curr => !curr); setError("") }} >{isSignUp ? "Existing user? Sign in" : "Not registered? Sign up"}</button>
+
+        {/* <button className={`${styles.loginOrSignUpButton} ${styles.right}`} onClick={handleClick}/>
+          {isLoading ? "Loading..." : isSignUp ? "Sign Up" : "Login"}
+        </button> */}
+
+        <button type="submit" className={`${styles.loginOrSignUpButton} ${styles.right}`} onClick={handleClick} >
           {isLoading ? "Loading..." : isSignUp ? "Sign Up" : "Login"}
         </button>
 
